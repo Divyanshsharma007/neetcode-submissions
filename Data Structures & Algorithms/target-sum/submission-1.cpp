e1021fe3935274901ef1739de1465f0f9d9276fa
Divyanshsharma007 <144471203+Divyanshsharma007@users.noreply.github.com>
2026-06-07 19:23:19 +0530

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) 
+    {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        if(abs(target) > totalSum)
+            return 0;
+
+        if((totalSum + target) % 2)
+            return 0;
+
+        int reqSum = (totalSum + target) / 2;
+
+        vector<int> dp(reqSum + 1, 0);
+        dp[0] = 1;
+
+        for(int num : nums)
+        {
+            for(int t = reqSum; t >= num; t--)
+            {
+                dp[t] += dp[t - num];
+            }
+        }
+
+        return dp[reqSum];
+    }
+
+   /* int solve(vector<int>&nums , int target , int  i , int sum)
+    {
+        int n=nums.size();
+        if(n == 0) return 0;
+        if(i == n)return sum==target;
+
+        return solve(nums ,target ,i+1 ,sum -nums[i]) + solve(nums , target ,i+1 , sum+ nums[i]);
+    }
+    */
+};
